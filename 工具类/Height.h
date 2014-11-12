80b5895c041b6146817f0234388ae6a8e3034197
HardingbearYK <xyk19921222@163.com>
2014-11-12 14:46:55 +0800

    new2014-11-12-2:46

---
+++ 工具类/Height.h
@@ -0,0 +1,13 @@
+//
+//  Height.h
+//  食材简谱
+//
+//  Created by 6006 on 14-10-12.
+//  Copyright (c) 2014年 ibokan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Height : NSObject
++(CGFloat)contentHeight:(NSString *)content andFontSize:(CGFloat)fontSize andWidth:(CGFloat) width;
+@end
